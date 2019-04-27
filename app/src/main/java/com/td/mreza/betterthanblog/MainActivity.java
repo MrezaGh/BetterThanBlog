33435698f970027f76387cc50220adfd14418a4a
MrezaGh <33687449+MrezaGh@users.noreply.github.com>
2019-04-27 15:48:37 +0430

    Base of the project
    all the things needed to start this project adapted from babytelesimulater project

---
+++ app/src/main/java/com/td/mreza/betterthanblog/MainActivity.java
@@ -0,0 +1,117 @@
+package com.td.mreza.betterthanblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//todo : classes should be singleton
+
+public class MainActivity extends AppCompatActivity implements Observer  {
+
+
+    LinearLayout linearLayout;
+    MessageController messageController;
+    NotificationCenter notificationCenter;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mehrdad);
+        notificationCenter = new NotificationCenter();
+        messageController = new MessageController(getBaseContext(), notificationCenter);
+        linearLayout = findViewById(R.id.linear_layout);
+        notificationCenter.register(this);
+
+//        messageController.fetch(false, 0);
+
+
+
+        Button clearBtn = findViewById(R.id.clear_btn);//done!
+        Button refreshBtn = findViewById(R.id.refresh_btn);//todo:
+        Button getBtn = findViewById(R.id.get_btn);//todo
+
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int lastNUmber;
+                if (messageController.cache.size() == 0)
+                    lastNUmber = 0;
+                else
+                    lastNUmber = messageController.cache.get(messageController.cache.size() - 1);
+                messageController.fetch(false, lastNUmber);
+            }
+        });
+
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int lastNUmber;
+                if (messageController.cache.size() == 0)
+                    lastNUmber = 0;
+                else
+                    lastNUmber = messageController.cache.get(messageController.cache.size() - 1);
+                messageController.fetch(true, lastNUmber);
+            }
+        });
+
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.removeAllViews();
+                messageController.cache.clear();
+            }
+        });
+
+
+    }
+    private String name;
+    private Subject topic;
+
+
+    @Override
+    public void update() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                MainActivity.this.updateList();
+            }
+        });
+
+
+    }
+
+    private void updateList() {
+        List<Integer> list =messageController.cache;
+        ArrayList<TextView> tvs = new ArrayList<>();
+        for (Integer s:list) {
+            TextView tv=new TextView(getApplicationContext());
+            tv.setText(s.toString());
+            tvs.add(tv);
+
+        }
+        Log.i("Threadd", Thread.currentThread().getName());
+        ArrayList<TextView> tvsNew = new ArrayList<>();
+        for (int i = 0; i <tvs.size() ; i++) {
+            if(i>tvs.size()-11) {
+                tvsNew.add(tvs.get(i));
+            }
+        }
+        for (TextView t: tvsNew) {
+            linearLayout.addView(t);
+        }
+    }
+
+    @Override
+    public void setSubject(Subject sub) {
+        this.topic=sub;
+    }
+}
---
+++ app/src/main/java/com/td/mreza/betterthanblog/StorageManager.java
@@ -0,0 +1,117 @@
+package com.td.mreza.betterthanblog;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
+public class StorageManager {
+    Context context;
+    File database;
+
+    public StorageManager(Context context){
+        this.context = context;
+        this.database = new File(context.getFilesDir(), "database");
+
+    }
+    public boolean save(final int lastNumber){
+        final CountDownLatch latch = new CountDownLatch(1);
+        final boolean[] saved = {false};
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("save ", "CurrentThread:" + Thread.currentThread().getName());
+                try {
+                    PrintWriter writer = new PrintWriter(database);
+                    writer.print(lastNumber);
+                    writer.close();
+                    saved[0] = true;
+                    latch.countDown();
+//                    Log.i("congrats: ", "saved");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Log.d("oops: ", "printer pokid");
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                    Log.d("oops: ", "printer pokid nullpointer");
+                }
+            }
+        };
+
+        Thread storage = new Thread(runnable, "storage thread save");
+        storage.start();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return saved[0];
+
+    }
+
+    public int[] load(final int lastNumber){
+        final int[] numbers = new int[10];
+        final CountDownLatch latch = new CountDownLatch(1);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("save ", "CurrentThread:" + Thread.currentThread().getName());
+                try {
+                    FileInputStream fileInputStream = context.openFileInput("database");
+                    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String lineData = bufferedReader.readLine();
+//                    Log.i("files -> ", "data: " + lineData);
+                    bufferedReader.close();
+                    if (lineData != null){
+                        int lastSavedNumber = Integer.parseInt(lineData);
+                        if (lastNumber < lastSavedNumber)
+                        for (int i = 0; i < 10; i++) {
+                            numbers[i] = i + lastNumber + 1;
+                        }
+                    }
+                    else if (lastNumber == 0){
+                        for (int i = 0; i < 10; i++) {
+                            numbers[i] = i + 1;
+                        }
+                    }
+                    latch.countDown();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Log.i("oops", "file not found");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.i("oops", "couldn't read file");
+                }
+
+            }
+        };
+
+        Thread storage = new Thread(runnable, "storage thread load");
+        storage.start();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (numbers[0] == 0)//if nothing needs to return numbers is {0,0,...}
+            return null;
+        return numbers;
+    }
+
+}
